4ac4d08f39e52f6e0ee73273407456f8beefee0b
Niket Tandon <nikett@allenai.org>
2024-05-06 13:55:15 -0700

    Revert supported models.

--- gptinference/openai_api.py
+++ gptinference/openai_api.py
@@ -57,7 +57,7 @@ def retry_with_exponential_backoff(
 
 
 def is_chat_based_agent(engine):
-    return engine.lower().strip() == "gpt-3.5-turbo" or engine.lower().strip() == "gpt-4" or engine.lower().strip() == "gpt-4-turbo"
+    return engine.lower().strip() == "gpt-3.5-turbo" or engine.lower().strip() == "gpt-4"
 
 
 class OpenaiAPIWrapper:
