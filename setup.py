192b2d2820d80a1e30c4d46e09e1725659504e80
nikett <nikett@allenai.org>
2024-05-07 18:26:39 -0700

    resolved openAI setup version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='callgpt',
-      version='0.1',
+      version='0.2',
       description="Library for running few-shot inference on GPT models",
       packages=["gptinference"],
       install_requires=[
-          'openai==0.28'
+          'openai==1.12.0'
       ],
       license='Apache License 2.0',
       long_description=open('README.md').read(),
