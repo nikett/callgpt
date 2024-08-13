78598334ab4da62338a5e25aded369283ee133a9
nikett <nikett@allenai.org>
2024-08-13 09:34:46 +0530

    removed support for old gpt models

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 
 setup(name='callgpt',
-      version='0.2',
+      version='0.21',
       description="Library for running few-shot inference on GPT models",
       packages=["gptinference"],
       install_requires=[
-          'openai==1.12.0'
+          'openai>=1.12.0'
       ],
       license='Apache License 2.0',
       long_description=open('README.md').read(),
